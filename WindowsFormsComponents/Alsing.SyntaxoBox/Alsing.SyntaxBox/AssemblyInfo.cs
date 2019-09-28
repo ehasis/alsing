441da4ea0ba67d6d90ea99d3cbb01ba362f6afce
Edison Henrique Andreassy <ehasis@hotmail.com>
2019-09-28 19:59:35 -0300

    Conversion of csproj's to SDK-Style

--- WindowsFormsComponents/Alsing.SyntaxoBox/Alsing.SyntaxBox/AssemblyInfo.cs
+++ WindowsFormsComponents/Alsing.SyntaxoBox/Alsing.SyntaxBox/AssemblyInfo.cs
@@ -1,32 +1,4 @@
-using System.Reflection;
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-
-[assembly : AssemblyTitle("Alsing SyntaxBox")]
-[assembly : AssemblyDescription("SyntaxHighlight Edit Control")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("Alsing")]
-[assembly : AssemblyProduct("SyntaxBox")]
-[assembly : AssemblyCopyright("2002-2003")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly : AssemblyVersion("1.4.10.*")]
+﻿using System.Reflection;
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
